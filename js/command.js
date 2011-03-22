064834f90c1833bf106b1960a3aad1d10dafd176
Adam Siegel <tobioatom@gmail.com>
2011-03-21 22:49:02 -0700

    added normal commands

---
+++ js/command.js
@@ -0,0 +1,135 @@
+var mode = {
+	"normal" : {
+		"_a_" : { //append after cursor
+		},
+		"_A_" : { //append at end of line
+		},
+		"_b_" : { //move cursor to the beginning of a word
+		},
+		"_B_" : { //move cursor to the beginning of a whitespace delimited word
+		},
+		"_b_" : { //move cursor to the beginning of a word
+		},
+		"_c_" : { //* change add more docs later
+		},
+		"_C_" : { //* change variant
+		},
+		"_d_" : { // delete text
+		},
+		"_D_" : { // d$
+		},
+		"_e_" : { // move the cursor to the end of a word
+		},
+		"_E_" : { // move the cursor to the end of a whitespace deliminated word
+		},
+		"_f_" : { // find the next char forward
+		},
+		"_F_" : { // find the next char backward
+		},
+		"_g_" : { //*
+		},
+		"_G_" : { // goto last line of text
+		},
+		"_h_" : {// cursor left 1
+		},
+		"_H_" : {// home window line
+		},
+		"_i_" : {// insert before cursor
+		},
+		"_I_" : {// insert at beginning of line
+		},
+		"_j_" : {// move cursor down
+		},
+		"_J_" : {// join line with line below it
+		},
+		"_k_" : {// move cursor up
+		},
+		"_K_" : {// man page
+		},
+		"_l_" : {// move cursor right 1
+		},
+		"_L_" : {// goto last line of screen
+		},
+		"_m_" : {//*
+		},
+		"_M_" : {// goto middle of screen
+		},
+		"_n_" : {// next occurance of pattern
+		},
+		"_N_" : {// prev occurance of pattern
+		},
+		"_o_" : {// open line below
+		},
+		"_O_" : {// openline above
+		},
+		"_p_" : {// paste after
+		},
+		"_P_" : {// paste before
+		},
+		"_q_" : {//*
+		},
+		"_Q_" : {// ex mode
+		},
+		"_r_" : {// overwrite current character with next character
+		},
+		"_R_" : {// replace mode
+		},
+		"_s_" : {// delete current character enter insert mode
+		},
+		"_S_" : {// delete current line and enter insert mode
+		},
+		"_t_" : {// search inline forward
+		},
+		"_T_" : {// search inline backward
+		},
+		"_u_" : {// undo
+		},
+		"_U_" : {// redo
+		},
+		"_v_" : {// start highlighting chars "visual mode"
+		},
+		"_V_" : {// start highlighting lines "visual mode"
+		},
+		"_w_" : {// forward 1 word
+		},
+		"_W_" : {// forward 1 word
+		},
+		"_x_" : {// delete current char
+		},
+		"_X_" : {// delete current char
+		},
+		"_y_" : {// yank
+		},
+		"_Y_" : {// yy
+		},
+		"_z_" : {// followed by cr or - puts current line at top or at bottom
+		},
+		"_Z_" : {// press twice exits and saves
+		},
+
+	},
+	"visual" : {
+	},
+	"select" : {
+	},
+	"insert" : {
+	},
+	"cmdline" : {
+	},
+	"ex" : {
+	},
+	"operator-pending" : {
+	},
+	"replace" : {
+	},
+	"vreplace" : {
+	},
+	"insert" : {
+		"normal" : {
+		},
+		"visual" : {
+		},
+		"select" : {
+		}
+	}
+}
